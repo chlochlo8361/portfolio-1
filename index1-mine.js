03269b0ac869a11f277b08c30ce36dd503a7d764
chlochlo8361 <cjaven@gmail.com>
2021-04-16 16:51:26 -0700

    made it work

--- index1-mine.js
+++ index1-mine.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', (event) => {
+    // Creating Constants
     const firstName = document.querySelector('#first')
     const lastName = document.querySelector('#last')
     const email = document.querySelector('#email')
@@ -6,34 +7,52 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const instagram = document.querySelector('#instagram')
     const loginButton = document.querySelector('#button')
 
-    // loginButton.addEventListener('click', validate)
+    // Event Listeners
+    loginButton.addEventListener('click', validate)
 
+    // Functions
     function validate() {
+        var passes = false;
+
         if(firstName.value.trim().length === 0) {
             firstName.style.border = '1px solid red';
+            passes = false;
         } else {
-            firstName.style.border = '0.063rem solid #ccc'
+            firstName.style.border = '0.063rem solid #ccc';
+            passes = true;
         }
         if(lastName.value.trim().length === 0) {
             lastName.style.border = '1px solid red';
+            passes = false;
         } else {
-            lastName.style.border = '0.063rem solid #ccc'
+            lastName.style.border = '0.063rem solid #ccc';
+            passes = true;
         }
         if(email.value.trim().length === 0) {
             email.style.border = '1px solid red';
+            passes = false;
         } else {
-            email.style.border = '0.063rem solid #ccc'
-        }
-        if(password.value.trim().length === 0) {
-            password.style.border = '1px solid red';
-        } else {
-            password.style.border = '0.063rem solid #ccc'
+            email.style.border = '0.063rem solid #ccc';
+            passes = true;
         }
         if(instagram.value.trim().length === 0) {
             instagram.style.border = '1px solid red';
+            passes = false;
         } else {
-            instagram.style.border = '0.063rem solid #ccc'
+            instagram.style.border = '0.063rem solid #ccc';
+            passes = true;
+        }
+        if(password.value === 'password') {
+            password.style.border = '0.063rem solid #ccc';
+            passes = true;
+        } else {
+            password.style.border = '1px solid red';
+            passes = false;
+        }
+        if(passes === true) {
+            window.open('index2.html');
+            console.log('You did it all areas are filled out')
         }
     }
-    loginButton.addEventListener('click', validate)
 });
+
